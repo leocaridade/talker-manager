426a418900249c675f86de9ca552afbd7a7e5380
leocaridade <leocaridade@hotmail.com>
2023-06-19 16:18:28 -0400

    Requisito 3 - create an endpoint POST /login

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { readTalkersData } = require('./utils/fsUtils');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,17 @@ app.get('/talker/:id', async (req, res) => {
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   return res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const token = generateToken();
+
+  if ([email, password].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  return res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
